69533e9aa26e35941740207a18b79ddd04a0e19f
xiaoxiong <zhu852514500@163.com>
2019-04-19 09:10:35 +0800

    档位选择控件，触摸事件实现原理，目标，给换挡添加音效

---
+++ app/src/main/java/com/saiyi/dangweiwidget/SizeUtils.java
@@ -0,0 +1,24 @@
+package com.saiyi.dangweiwidget;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.TypedValue;
+
+public class SizeUtils {
+    public static int dp2px(Context context, float dpValue) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpValue, context.getResources().getDisplayMetrics());
+    }
+
+    public static int sp2px(Context context, float spValue) {
+        return (int) (spValue * context.getResources().getDisplayMetrics().scaledDensity + 0.5f);
+    }
+
+    public static int px2sp(Context context, float pxValue) {
+        return (int) (pxValue / context.getResources().getDisplayMetrics().scaledDensity + 0.5f);
+    }
+
+    public static int px2dp(float pxValue) {
+        final float scale = Resources.getSystem().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+}
